0d61019475b73c490cae56e31bd1dc175d96976a
randomsomeone69-real <160155909+randomsomeone69-real@users.noreply.github.com>
2026-03-16 17:03:13 +0100

    forgor to upload the program.cs file :skull:
    please git bash work without doxxing myself

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+(int, int) conv_trt(int Min)
+{
+    int H, M;
+    H = Min / 60;
+    M = Min % 60;
+    return (H,M);
+}
+
+(int H, int M) = conv_trt(90);
+Console.WriteLine($"The tape lasts {H}:{M:D2} of contiguous time.");
